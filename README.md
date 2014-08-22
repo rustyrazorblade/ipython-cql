c26044c46b10dd90697010d6505c4e97526da5a0
Jon Haddad <jon@jonhaddad.com>
2014-08-22 13:04:31 -0700

    usage docs

---
+++ README.md
@@ -0,0 +1,18 @@
+This extension is based on the work done by Catherine Devlin: https://github.com/catherinedevlin/ipython-sql
+
+For now it automatically connects to localhost.
+
+Load the extension:
+    
+    %load_ext cql
+    
+Select a keyspace:
+
+    %keyspace tutorial
+    
+Execute CQL Statements
+
+    %cql select * from user;
+
+
+
--- cql/__init__.py
+++ cql/__init__.py
@@ -1,6 +1,38 @@
+from IPython.core.magic import Magics, magics_class, cell_magic, line_magic, needs_local_scope
+from IPython.config.configurable import Configurable
+from cassandra.cluster import Cluster
+
+cluster = None
+session = None
 
 def load_ipython_extension(ipython):
-    print "HI"
+
+    global cluster, session
+
+    cluster = Cluster()
+    session = cluster.connect()
+
+    ipython.register_magics(CQLMagic)
+
 
 def unload_ipython_extension(ipython):
-    print "BYE"
+    pass
+
+
+
+@magics_class
+class CQLMagic(Magics, Configurable):
+    @needs_local_scope
+    @line_magic('cql')
+    @cell_magic('cql')
+    def execute(self, line, cell="", local_ns=None):
+        global session
+        result = session.execute(line)
+        return result
+
+    @line_magic("keyspace")
+    def set_keyspace(self, line, cell="", local_ns=None):
+        print "Using keyspace %s" % line
+        session.set_keyspace(line)
+
+
