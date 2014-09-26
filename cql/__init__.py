d8e1dc09bfab2ab898a3508a0ac861c20ed433d1
Jon Haddad <jon@jonhaddad.com>
2014-09-26 13:41:34 -0700

    getting histograms to work

--- cql/__init__.py
+++ cql/__init__.py
@@ -1,8 +1,15 @@
 from IPython.core.magic import Magics, magics_class, cell_magic, line_magic, needs_local_scope
 from IPython.config.configurable import Configurable
 from cassandra.cluster import Cluster
-from cassandra.query import ordered_dict_factory
+from cassandra.query import ordered_dict_factory, SimpleStatement
 from prettytable import PrettyTable
+
+try:
+    import numpy as np
+    import matplotlib.mlab as mlab
+    import matplotlib.pyplot as plt
+except:
+    pass
 
 cluster = None
 session = None
@@ -47,6 +54,15 @@ class CQLResult(object):
         table = self.get_table()
         return table.get_string()
 
+class TablePrinter(object):
+    def __init__(self, table):
+        self.table = table
+
+    def _repr_html_(self):
+        return self.table.get_html_string()
+
+    def __repr__(self):
+        return self.table.get_string()
 
 
 @magics_class
@@ -67,7 +83,73 @@ class CQLMagic(Magics, Configurable):
         print "Using keyspace %s" % line
         session.set_keyspace(line)
 
-
     @line_magic("tables")
     def get_tables(self, line, cell="", local_ns=None):
-        return cluster.metadata.keyspaces[session.keyspace].tables.keys()
+        table = PrettyTable(["name", "partition key", "clustering key", "compaction"])
+
+        for name, tab in cluster.metadata.keyspaces[session.keyspace].tables.iteritems():
+            pks = ",".join([x.name for x in tab.partition_key])
+            clustering_key = ",".join([x.name for x in tab.clustering_key])
+
+            compaction = tab.options['compaction_strategy_class'].replace("org.apache.cassandra.db.compaction.", "")
+
+            table.add_row([name, pks, clustering_key, compaction])
+
+        return TablePrinter(table)
+
+        # return cluster.metadata.keyspaces[session.keyspace].tables.keys()
+
+
+    @line_magic("histogram")
+    def get_histogram(self, line, cell="", local_ns=None):
+
+
+        mu, sigma = 100, 15
+        x = mu + sigma*np.random.randn(10000)
+
+        # the histogram of the data
+        global session
+        result = session.query(line)
+        print result
+        n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+
+        # add a 'best fit' line
+        y = mlab.normpdf( bins, mu, sigma)
+        l = plt.plot(bins, y, 'r--', linewidth=1)
+
+        plt.xlabel('Smarts')
+        plt.ylabel('Probability')
+        plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
+        plt.axis([40, 160, 0, 0.03])
+        plt.grid(True)
+        plt.show()
+
+
+    @line_magic("trace")
+    def get_trace(self, line, cell="", local_ns=None):
+
+        global session
+
+        if cell:
+            line = cell
+
+        query = SimpleStatement(line)
+        result = session.execute(query, trace=True)
+        return Trace(query)
+
+
+class Trace(object):
+    def __init__(self, query):
+        # accepts a simple statement
+        self.trace = query.trace
+
+    def get_table(self):
+        table = PrettyTable(["activity", "timestamp", "source", "source_elapsed"])
+        for event in self.trace.events:
+            table.add_row([event.description, event.datetime, event.source, event.source_elapsed])
+
+        return table
+
+    def _repr_html_(self):
+        table = self.get_table()
+        return table.get_html_string()
